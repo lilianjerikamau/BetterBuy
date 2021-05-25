e59b4f5628e59215daf91e95cc2f55ce15e9b044
lilian <you@example.com>
2021-05-25 14:36:13 +0300

    twitter login

--- app/src/main/java/com/example/betterbuy/HomeActivity.java
+++ app/src/main/java/com/example/betterbuy/HomeActivity.java
@@ -1,33 +1,65 @@
 package com.example.betterbuy;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-import retrofit2.Call;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.twitter.sdk.android.core.SessionManager;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterSession;
 
 public class HomeActivity extends AppCompatActivity {
     TweeterApi client = TweeterClient.getClient();
 
 
-private Button LogoutBtn;
+
+    Button logoutBtn;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_second);
 
-        LogoutBtn = (Button) findViewById(R.id.logout_btn);
+        //Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        //Logout Btn
+        logoutBtn = findViewById(R.id.logout_button);
 
-        LogoutBtn.setOnClickListener(new View.OnClickListener() {
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(HomeActivity.this,MainActivity.class);
-                startActivity(intent);
+                SessionManager<TwitterSession> sessionManager = TwitterCore.getInstance().getSessionManager();
+                if (sessionManager.getActiveSession() != null){
+                    sessionManager.clearActiveSession();
+                    mAuth.signOut();
+                }
+
+                mAuth.signOut();
+                updateUI();
             }
         });
+    }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null) {
+            updateUI();
+        }
+    }
+
+    private void updateUI() {
+        Toast.makeText(HomeActivity.this, "You're logged out", Toast.LENGTH_LONG);
+
+        Intent mainActivity = new Intent(HomeActivity.this, LoginActivity.class);
+        startActivity(mainActivity);
+        finish();
     }
 }
--- app/src/main/java/com/example/betterbuy/TwitterActivity.java
+++ app/src/main/java/com/example/betterbuy/TwitterActivity.java
@@ -1,103 +1,103 @@
-package com.example.betterbuy;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import retrofit2.Call;
-import retrofit2.Callback;
-
-public class TwitterActivity extends AppCompatActivity {
-    private static final String TAG = TwitterActivity.class.getSimpleName();
-
-    @BindView(R.id.locationTextView) TextView mLocationTextView;
-    @BindView(R.id.listView) ListView mListView;
-    @BindView(R.id.errorTextView) TextView mErrorTextView;
-    @BindView(R.id.progressBar) ProgressBar mProgressBar;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_twitter);
-        ButterKnife.bind(this);
-
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String restaurant = ((TextView)view).getText().toString();
-                Toast.makeText(TwitterActivity.this, restaurant, Toast.LENGTH_LONG).show();
-            }
-        });
-
-        Intent intent = getIntent();
-        String result_type = intent.getStringExtra("location");
-        mLocationTextView.setText("Here are all the restaurants near: " + location);
-
-        TweeterApi client = TweeterClient.getClient();
-
-        Call<TweeterTweetSearchResponse> call = client.getTweets(result_type, "restaurants");
-
-        call.enqueue(new Callback<TweeterTweetSearchResponse>() {
-            @Override
-            public void onResponse(Call<YelpBusinessesSearchResponse> call, Response<YelpBusinessesSearchResponse> response) {
-                hideProgressBar();
-
-                if (response.isSuccessful()) {
-                    List<Business> restaurantsList = response.body().getBusinesses();
-                    String[] restaurants = new String[restaurantsList.size()];
-                    String[] categories = new String[restaurantsList.size()];
-
-                    for (int i = 0; i < restaurants.length; i++){
-                        restaurants[i] = restaurantsList.get(i).getName();
-                    }
-
-                    for (int i = 0; i < categories.length; i++) {
-                        Category category = restaurantsList.get(i).getCategories().get(0);
-                        categories[i] = category.getTitle();
-                    }
-
-                    ArrayAdapter adapter
-                            = new MyRestaurantsArrayAdapter(RestaurantsActivity.this, android.R.layout.simple_list_item_1, restaurants, categories);
-                    mListView.setAdapter(adapter);
-
-                    showRestaurants();
-                } else {
-                    showUnsuccessfulMessage();
-                }
-            }
-
-            @Override
-            public void onFailure(Call<YelpBusinessesSearchResponse> call, Throwable t) {
-                hideProgressBar();
-                showFailureMessage();
-            }
-
-        });
-    }
-
-    private void showFailureMessage() {
-        mErrorTextView.setText("Something went wrong. Please check your Internet connection and try again later");
-        mErrorTextView.setVisibility(View.VISIBLE);
-    }
-
-    private void showUnsuccessfulMessage() {
-        mErrorTextView.setText("Something went wrong. Please try again later");
-        mErrorTextView.setVisibility(View.VISIBLE);
-    }
-
-    private void showRestaurants() {
-        mListView.setVisibility(View.VISIBLE);
-        mLocationTextView.setVisibility(View.VISIBLE);
-    }
-
-    private void hideProgressBar() {
-        mProgressBar.setVisibility(View.GONE);
-    }
-    }
+//package com.example.betterbuy;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.AdapterView;
+//import android.widget.TextView;
+//import android.widget.Toast;
+//
+//import butterknife.BindView;
+//import butterknife.ButterKnife;
+//import retrofit2.Call;
+//import retrofit2.Callback;
+//
+//public class TwitterActivity extends AppCompatActivity {
+//    private static final String TAG = TwitterActivity.class.getSimpleName();
+//
+//    @BindView(R.id.locationTextView) TextView mLocationTextView;
+//    @BindView(R.id.listView) ListView mListView;
+//    @BindView(R.id.errorTextView) TextView mErrorTextView;
+//    @BindView(R.id.progressBar) ProgressBar mProgressBar;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_twitter);
+//        ButterKnife.bind(this);
+//
+//        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                String restaurant = ((TextView)view).getText().toString();
+//                Toast.makeText(TwitterActivity.this, restaurant, Toast.LENGTH_LONG).show();
+//            }
+//        });
+//
+//        Intent intent = getIntent();
+//        String result_type = intent.getStringExtra("location");
+//        mLocationTextView.setText("Here are all the restaurants near: " + location);
+//
+//        TweeterApi client = TweeterClient.getClient();
+//
+//        Call<TweeterTweetSearchResponse> call = client.getTweets(result_type, "restaurants");
+//
+//        call.enqueue(new Callback<TweeterTweetSearchResponse>() {
+//            @Override
+//            public void onResponse(Call<YelpBusinessesSearchResponse> call, Response<YelpBusinessesSearchResponse> response) {
+//                hideProgressBar();
+//
+//                if (response.isSuccessful()) {
+//                    List<Business> restaurantsList = response.body().getBusinesses();
+//                    String[] restaurants = new String[restaurantsList.size()];
+//                    String[] categories = new String[restaurantsList.size()];
+//
+//                    for (int i = 0; i < restaurants.length; i++){
+//                        restaurants[i] = restaurantsList.get(i).getName();
+//                    }
+//
+//                    for (int i = 0; i < categories.length; i++) {
+//                        Category category = restaurantsList.get(i).getCategories().get(0);
+//                        categories[i] = category.getTitle();
+//                    }
+//
+//                    ArrayAdapter adapter
+//                            = new MyRestaurantsArrayAdapter(RestaurantsActivity.this, android.R.layout.simple_list_item_1, restaurants, categories);
+//                    mListView.setAdapter(adapter);
+//
+//                    showRestaurants();
+//                } else {
+//                    showUnsuccessfulMessage();
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<YelpBusinessesSearchResponse> call, Throwable t) {
+//                hideProgressBar();
+//                showFailureMessage();
+//            }
+//
+//        });
+//    }
+//
+//    private void showFailureMessage() {
+//        mErrorTextView.setText("Something went wrong. Please check your Internet connection and try again later");
+//        mErrorTextView.setVisibility(View.VISIBLE);
+//    }
+//
+//    private void showUnsuccessfulMessage() {
+//        mErrorTextView.setText("Something went wrong. Please try again later");
+//        mErrorTextView.setVisibility(View.VISIBLE);
+//    }
+//
+//    private void showRestaurants() {
+//        mListView.setVisibility(View.VISIBLE);
+//        mLocationTextView.setVisibility(View.VISIBLE);
+//    }
+//
+//    private void hideProgressBar() {
+//        mProgressBar.setVisibility(View.GONE);
+//    }
+//    }
