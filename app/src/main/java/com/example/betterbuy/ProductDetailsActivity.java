75e5c4447f84fbfa1a8f58cd900d2fc31c4d187c
lilian <you@example.com>
2021-06-07 11:13:41 +0300

    add to cart

--- app/src/main/java/com/example/betterbuy/ProductDetailsActivity.java
+++ app/src/main/java/com/example/betterbuy/ProductDetailsActivity.java
@@ -1,13 +1,19 @@
 package com.example.betterbuy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -17,14 +23,20 @@ import com.squareup.picasso.Picasso;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+
 import Model.Products;
+import prevalent.Prevalent;
 
 public class ProductDetailsActivity extends AppCompatActivity {
 
-//    private FloatingActionButton addToCartBtn;
+
     private ImageView productImage;
     private ElegantNumberButton numberButton;
     private TextView productPrice,productDescription,productName;
+    private Button addToCartButton;
     private String productID = "";
 
     @Override
@@ -33,15 +45,69 @@ public class ProductDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_product_details);
         productID = getIntent().getStringExtra("pid");
 
-//        addToCartBtn = (FloatingActionButton) findViewById(R.id.add_product_to_cart_btn);
+
         numberButton = (ElegantNumberButton) findViewById(R.id.number_btn);
         productImage = (ImageView) findViewById(R.id.product_image_details);
         productName = (TextView) findViewById(R.id.product_name_details);
         productDescription = (TextView) findViewById(R.id.product_description_details);
         productPrice = (TextView) findViewById(R.id.product_price_details);
+        addToCartButton = (Button) findViewById(R.id.pd_add_to_cart);
 
         getProductDetails(productID);
 
+        addToCartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                addingToCartList();
+            }
+        });
+
+
+    }
+
+    private void addingToCartList()
+    {
+        String saveCurrentTime, saveCurrentDate;
+        Calendar callForDate =  Calendar.getInstance();
+        SimpleDateFormat currentDate = new SimpleDateFormat("MMM dd,yyyy");
+        saveCurrentDate = currentDate.format(callForDate.getTime());
+        SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss a");
+        saveCurrentTime = currentDate.format(callForDate.getTime());
+
+       final DatabaseReference cartListRef = FirebaseDatabase.getInstance().getReference().child("Cart List");
+        final HashMap<String ,Object> cartMap = new HashMap<>();
+        cartMap.put("pid",productID);
+        cartMap.put("pname",productName.getText().toString());
+        cartMap.put("price",productPrice.getText().toString());
+        cartMap.put("time",saveCurrentTime);
+        cartMap.put("date",saveCurrentDate);
+        cartMap.put("quantity",numberButton.getNumber());
+        cartMap.put("discount","");
+
+        cartListRef.child("User View").child(Prevalent.currentOnlineUser.getPhone()).child("Products").child(productID).updateChildren(cartMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull @NotNull Task<Void> task)
+            {
+               if (task.isSuccessful())
+               {
+                   cartListRef.child("Admin View").child(Prevalent.currentOnlineUser.getPhone()).child("Products").child(productID).updateChildren(cartMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                       @Override
+                       public void onComplete(@NonNull @NotNull Task<Void> task)
+                       {
+                           if(task.isSuccessful())
+                           {
+                               Toast.makeText(ProductDetailsActivity.this,"Item added to cart successfully",Toast.LENGTH_SHORT).show();
+                               Intent intent = new Intent (ProductDetailsActivity.this,HomeActivity.class);
+                               startActivity(intent);
+
+                           }
+
+                       }
+                   });
+               }
+            }
+        });
 
     }
 
